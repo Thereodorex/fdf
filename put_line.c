98359746210eebf05047114b8b8081a0bc2163fb
hchau <ifaley@bk.ru>
2019-12-29 00:52:51 +0300

    gradient

--- put_line.c
+++ put_line.c
@@ -1,5 +1,6 @@
 #include "header.h"
 #include <stdio.h>
+#include <math.h>
 
 /*
 **			алгоритм Брезенхайма
@@ -9,11 +10,6 @@ static int 		get_next_coordinate(int delta, int dirx, int distortion, int *x1)
 {
 	static int 	error = 0;
 
-	if (distortion == -1)
-	{
-		error = 0;
-		return (0);
-	}
 	error += distortion;
 	if (error * 2 >= delta)
 	{
@@ -23,30 +19,65 @@ static int 		get_next_coordinate(int delta, int dirx, int distortion, int *x1)
 	return (1);
 }
 
+double percent(int start, int end, int current)
+{
+	double placement;
+	double distance;
+
+	placement = current - start;
+	distance = end - start;
+	return ((distance == 0) ? 1.0 : (placement / distance));
+}
+
+static int		get_light(int start, int end, double percentage)
+{
+	return ((int)((1 - percentage) * start + percentage * end));
+}
+
+int			find_gradient(t_coord current, t_coord start, t_coord end, t_coord delta)
+{
+	int     red;
+	int     green;
+	int     blue;
+	double  percentage;
+
+	if (current.color == end.color)
+		return (current.color);
+	if (delta.x > delta.y)
+		percentage = percent(start.x, end.x, current.x);
+	else
+		percentage = percent(start.y, end.y, current.y);
+	red = get_light((start.color >> 16) & 0xFF, (end.color >> 16) & 0xFF, percentage);
+	green = get_light((start.color >> 8) & 0xFF, (end.color >> 8) & 0xFF, percentage);
+	blue = get_light(start.color & 0xFF, end.color & 0xFF, percentage);
+	return ((red << 16) | (green << 8) | blue);
+}
+
 void 		put_line(t_coord first, t_coord second, t_param *param)
 {
-	int 	deltax;
-	int 	deltay;
-	int 	y_direction;
-	int 	x_direction;
+	t_coord		delta;
+	t_coord		direction;
+	t_coord		new;
 
-	deltax = second.x - first.x < 0 ? first.x - second.x : second.x - first.x;
-	deltay = second.y - first.y < 0 ? first.y - second.y : second.y - first.y;
-	y_direction = second.y > first.y ? 1 : -1;
-	x_direction = second.x > first.x ? 1 : -1;
-	get_next_coordinate(0, 0, -1, 0);
-	if (deltay < deltax)
-		while (first.x != second.x)
+	new = first;
+	delta.x = second.x - first.x < 0 ? first.x - second.x : second.x - first.x;
+	delta.y = second.y - first.y < 0 ? first.y - second.y : second.y - first.y;
+	direction.y = second.y > first.y ? 1 : -1;
+	direction.x = second.x > first.x ? 1 : -1;
+	if (delta.y < delta.x)
+		while (new.x != second.x)
 		{
-			mlx_pixel_put(param->mlx_ptr, param->win_ptr, first.x, first.y, first.color);
-			get_next_coordinate(deltax, y_direction, deltay, &first.y);
-			first.x += x_direction;
+			mlx_pixel_put(param->mlx_ptr, param->win_ptr, new.x, new.y, new.color);
+			get_next_coordinate(delta.x, direction.y, delta.y, &new.y);
+			new.x += direction.x;
+			new.color = find_gradient(new, first, second, delta);
 		}
 	else
-		while (first.y != second.y)
+		while (new.y != second.y)
 		{
-			mlx_pixel_put(param->mlx_ptr, param->win_ptr, first.x, first.y, first.color);
-			(get_next_coordinate(deltay, x_direction, deltax, &first.x));
-			first.y += y_direction;
+			mlx_pixel_put(param->mlx_ptr, param->win_ptr, new.x, new.y, new.color);
+			get_next_coordinate(delta.y, direction.x, delta.x, &new.x);
+			new.y += direction.y;
+			new.color = find_gradient(new, first, second, delta);
 		}
 }
