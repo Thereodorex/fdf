8714d85161cc25b0ceec0ccdf74fb3d75762db7c
Jamil Corwin <jcorwin@at-j5.21-school.ru>
2019-12-28 18:30:47 +0300

    not so good rotation

--- rotate.c
+++ rotate.c
@@ -19,8 +19,36 @@
 
 // 	x = node->prime.x - SIZE_WINDOW / 2;
 // 	y = node->prime.y - SIZE_WINDOW / 2;
-// 	node->current.x = (double)node->prime.x * cos(param->z_rot) - (double)node->prime.y * sin(param->z_rot);
-// 	node->current.y = (double)node->prime.x * sin(param->z_rot) + (double)node->prime.y * cos(param->z_rot);
+// 	node->current.x = (double)x * cos(param->z_rot) - (double)y * sin(param->z_rot) + SIZE_WINDOW / 2;
+// 	node->current.y = (double)x * sin(param->z_rot) + (double)y * cos(param->z_rot) + SIZE_WINDOW / 2;
+// }
+
+// void	rotate_node(t_node *node, t_param *param)
+// {
+// 	int		x;
+// 	int		y;
+// 	int		z;
+
+// 	x = node->prime.x - SIZE_WINDOW / 2;
+// 	y = node->prime.y - SIZE_WINDOW / 2;
+// 	z = node->prime.z;
+
+// 	node->current.x = x;
+// 	node->current.y = y;
+
+
+// 	// node->current.y = (double)y * cos(param->x_rot) + (double)z * sin(param->x_rot);
+// 	// node->current.z = (double)z * cos(param->x_rot) - (double)y * sin(param->x_rot);	
+
+// 	// node->current.x = (double)x * cos(param->y_rot) + (double)node->current.z * sin(param->y_rot);
+// 	// node->current.z = (double)node->current.z * cos(param->y_rot) - (double)x * sin(param->y_rot);
+
+// 	node->current.x = (double)node->current.x * cos(param->z_rot) - (double)node->current.y  * sin(param->z_rot) + SIZE_WINDOW / 2;
+// 	node->current.y = (double)x * sin(param->z_rot) + (double)y  * cos(param->z_rot) + SIZE_WINDOW / 2;
+
+// 	node->current.x += param->x_move;
+// 	node->current.y += param->y_move;
+
 // }
 
 void	rotate_node(t_node *node, t_param *param)
@@ -33,17 +61,25 @@ void	rotate_node(t_node *node, t_param *param)
 	y = node->prime.y - SIZE_WINDOW / 2;
 	z = node->prime.z;
 
-	node->current.x = node->prime.x;
-	node->current.y = node->prime.y;
+	node->current.x = x;
+	node->current.y = y;
 
 	node->current.y = (double)y * cos(param->x_rot) + (double)z * sin(param->x_rot);
 	node->current.z = (double)z * cos(param->x_rot) - (double)y * sin(param->x_rot);
 
-	node->current.x = (double)x * cos(param->y_rot) + (double)node->current.z * sin(param->y_rot);
-	node->current.z = (double)node->current.z * cos(param->y_rot) - (double)x * sin(param->y_rot);
+	y = node->current.y;
+	z = node->current.z;
+		x = node->current.x;
 
-	node->current.x = (double)node->current.x * cos(param->z_rot) - (double)node->current.y  * sin(param->z_rot);
-	node->current.y = (double)node->current.x * sin(param->z_rot) + (double)node->current.y  * cos(param->z_rot);
+	node->current.x = (double)x * cos(param->z_rot) - (double)y  * sin(param->z_rot);
+	node->current.y = (double)x * sin(param->z_rot) + (double)y  * cos(param->z_rot);
+
+	x = node->current.x;
+	y = node->current.y;
+	z = node->current.z;
+
+	node->current.x = (double)x * cos(param->y_rot) + (double)z * sin(param->y_rot);
+	node->current.z = (double)z * cos(param->y_rot) - (double)x * sin(param->y_rot);
 
 	node->current.x += param->x_move + SIZE_WINDOW / 2;
 	node->current.y += param->y_move + SIZE_WINDOW / 2;
